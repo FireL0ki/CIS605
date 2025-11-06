b28ff2c27a3de8d018420d2b07fab95aa8d7d89a
Sif Oberon <muell758@d.umn.edu>
2025-11-06 16:40:33 -0600

    assignment debugging and fixes - program 11 & water_tank module

--- Assignments/AssignmentSet5/program11.py
+++ Assignments/AssignmentSet5/program11.py
@@ -1,11 +1,12 @@
 # Description: Gets user inputs to fill or drain a water tank
 # Developer: Sif Oberon
 # Date Created: 10.28.2025
-# Date Last Modified: 10.31.2025
+# Date Last Modified: 11.6.2025
 
 
 # imports the Water_Tank class from the water_tank module
 from water_tank import Water_Tank
+from sys import exit
 
 # declare a module level variable (for referencing a water tank object) initialized to “None”
 a_water_tank = None
@@ -20,16 +21,14 @@ def main():
 # (radius – int, minimum: 5, maximum: 50; depth – int, minimum: 10, maximum: 100)
 def get_user_input_for_watertank():
     try:
-        # always true
         while True:
-            radius = input(int("Enter a tank radius between 5 and 50: "))
+            radius = int(input("Enter a tank radius between 5 and 50: "))
             # check radius is within range, if it is, terminate loop with break
-            if radius >= 50 and radius >= 5: break
+            if 5 <= radius <= 50: break
         while True:
-            depth = input(int("Enter a tank depth between 10 and 100: "))
+            depth = int(input("Enter a tank depth between 10 and 100: "))
             # check depth is within range
-            if depth >= 100 and depth >= 10: break
-
+            if 10 <= depth <= 100: break
     except:
         print("Input Error")
         # run get use inputs again to get correct values
@@ -71,8 +70,9 @@ def display_menu():
             if 1 <= user_selection <= 7: break
     except:
         print("Input error")
-        # call method to get user selection
-        call_menu_function(user_selection)
+
+    # call method to get user selection
+    call_menu_function(user_selection)
 
 # method to call appropriate function based on user selection
 def call_menu_function(user_selection):
@@ -80,20 +80,17 @@ def call_menu_function(user_selection):
         case 1: print_watertank_object()
         case 2: add_water_to_tank()
         case 3: withdraw_water_from_tank()
-        case 4: Water_Tank.fill_water_per_second()
-        case 5: Water_Tank.drain_water_per_second()
-        case 6: get_user_input_for_watertank()
-        case 7: exit_program()
-
-def exit_program():
-    print("Program complete")
+        case 4: fill_water_per_second()
+        case 5: drain_water_per_second()
+        case 6: create_additional_watertank()
+        case 7: exit_application()
 
 # function to get user input (using an appropriate prompt and validation) for the gallons of water to add
 # to the tank (int, minimum: 1, maximum: 6,000,000)
 def add_water_to_tank():
     try:
         while True:
-            gallons_water_to_add = input(int("Enter the number of gallons of water to add (between 1 and 6,000,000): "))
+            gallons_water_to_add = int(input("Enter the number of gallons of water to add (between 1 and 6,000,000): "))
             # if user input is within range, break out of loop
             if 1 <= gallons_water_to_add <= 6000000: break
     except:
@@ -101,7 +98,7 @@ def add_water_to_tank():
         add_water_to_tank()
     else:
         # call the method to add water and display the returned message
-        print(a_water_tank.add_water_to_tank())
+        print(a_water_tank.add_water_to_tank(gallons_water_to_add))
     # call the function to display the menu
     display_menu()
 
@@ -110,15 +107,15 @@ def add_water_to_tank():
 def withdraw_water_from_tank():
     try:
         while True:
-            gallons_water_to_withdraw = input(int("Enter the number of gallons of water to withdraw (between 1 and 6,000,000): "))
+            gallons_water_to_withdraw = int(input("Enter the number of gallons of water to withdraw (between 1 and 6,000,000): "))
             # if user input is within range, break out of loop
             if 1 <= gallons_water_to_withdraw <= 6000000: break
     except:
         print("Input error")
         withdraw_water_from_tank()
     else:
-        # call the method to add water and display the returned message
-        print(a_water_tank.withdraw_water_from_tank())
+        # call the method to withdraw water and display the returned message
+        print(a_water_tank.withdraw_water_from_tank(gallons_water_to_withdraw))
     # call the function to display the menu
     display_menu()
 
@@ -127,234 +124,69 @@ def withdraw_water_from_tank():
 def fill_water_per_second():
     try:
         while True:
-            gallons_to_add_per_second = input(int("Enter the number of gallons of water to fill per second (between 1 and 6,000,000): "))
+            gallons_to_add_per_second = int(input("Enter the number of gallons of water to fill per second (between 1 and 6,000,000): "))
             # if user input is within range, break out of loop
             if 1 <= gallons_to_add_per_second <= 6000000: break
     except:
         print("Input error")
         fill_water_per_second()
     else:
-        # call the method to add water and display the returned message
-        print(a_water_tank.fill_water_per_second())
+        # call the method to fill water and display the returned message
+        while a_water_tank.fill_water_per_second(gallons_to_add_per_second):
+            # print current water level each loop
+            print(f"{a_water_tank.current_water_level_gallons:,} gallons")
+
+        # print message to user when tank is full
+        print(f"Tank is either full or cannot add another {gallons_to_add_per_second:,} gallons")
     # call the function to display the menu
     display_menu()
 
-# b. call the method to fill water within the body of a loop; to keep things simple, assume each
-# loop iteration takes a second; the loop should execute as long as the method returns a
-# value of true; after each iteration display the current water level of the tank
-# c. call the function (#6) to display the menu
-
-# 18. Has a function to
-# a. get user input (using an appropriate prompt and validation) for the rate (i.e.,
+# function to get user input (using an appropriate prompt and validation) for the rate (i.e.,
 # gallons/second) at which to drain the tank (int, minimum: 1, maximum: 5000)
-# b. call the method to drain water within the body of a loop; to keep things simple, assume
-# each loop iteration takes a second; the loop should execute as long as the method returns
-# a value of true; after each iteration display the current water level of the tank
-# c. call the function (#6) to display the menu
+def drain_water_per_second():
+    try:
+        while True:
+            gallons_to_drain_per_second = int(input("Enter the number of gallons of water to drain per second (between 1 and 6,000,000): "))
+            # if user input is within range, break out of loop
+            if 1 <= gallons_to_drain_per_second <= 6000000: break
+    except:
+        print("Input error")
+        drain_water_per_second()
+    else:
+        # call the method to drain water within the body of a loop; to keep things simple, assume
+        # each loop iteration takes a second; the loop should execute as long as the method returns
+        # a value of true; after each iteration display the current water level of the tank
+        while a_water_tank.drain_water_per_second(gallons_to_drain_per_second):
+            print(f"{a_water_tank.current_water_level_gallons:,} gallons")
+    
+    # when loop is finished due to tank capacity, print message to user
+    print(f"Tank is either empty or cannot drain another {gallons_to_drain_per_second:,} gallons")
 
-# 19. Has a function to
-# a. ask the user if they wish to create another water tank object
-# b. if yes, call the function (#4) that gets input for a water tank object
-# c. else, call the function (#6) to display the menu
+    # call the function to display the menu
+    display_menu()
 
-# 20. Has a function to
-# a. ask the user if they wish to exit the application
-# b. if yes, exit the application (hint: import the sys module and use the exit function)
-# c. else, call the function (#6) to display the menu
+
+# function to ask the user if they wish to create another water tank object
+# b. if yes, call the function that gets input for a water tank object
+def create_additional_watertank():
+    user_response = input("Would you like to create another water tank object? (Y / N): ")
+
+    if user_response.upper() == "Y":
+        get_user_input_for_watertank()
+    else:
+        # else, call the function to display the menu
+        display_menu()
+
+# function to ask the user if they wish to exit the application
+def exit_application():
+    user_response = input("Would you like to exit the application? (Y / N): ")
+
+    if user_response.upper() == "Y":
+        # if yes, exit the application
+        exit()
+    else:
+        # else, call the function to display the menu
+        display_menu()
 
 # 21. Calls the “main” function
 main()
-
-
-# TODO sample output
-# Water Tank By LV
-# Enter a radius between 5 and 50 feet: 10
-# Enter a depth between 10 and 100 feet: 20
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 1
-# Tank radius: 10 feet
-# Tank depth: 20 feet
-# Max capacity: 46,998 gallons
-# Current water level: 0 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 2
-# Enter the gallons of water to add to the tank (1-6,000,000): 20000
-# 20,000 gallons of water added
-# Current water level of tank: 20,000 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 3
-# Enter the gallons of water to withdraw from the tank (1-6,000,000): 10000
-# 10,000 gallons of water withdrawn
-# Current water level of tank: 10,000 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 4
-# Enter the rate (gallons of water/second) to fill the tank (1-5,000): 1000
-# 11,000 gallons
-# 12,000 gallons
-# 13,000 gallons
-# 14,000 gallons
-# 15,000 gallons
-# 16,000 gallons
-# 17,000 gallons
-# 18,000 gallons
-# 19,000 gallons
-# 20,000 gallons
-# 21,000 gallons
-# 22,000 gallons
-# 23,000 gallons
-# 24,000 gallons
-# 25,000 gallons
-# 26,000 gallons
-# 27,000 gallons
-# 28,000 gallons
-# 29,000 gallons
-# 30,000 gallons
-# 31,000 gallons
-# 32,000 gallons
-# 33,000 gallons
-# 34,000 gallons
-# 35,000 gallons
-# 36,000 gallons
-# 37,000 gallons
-# 38,000 gallons
-# 39,000 gallons
-# 40,000 gallons
-# 41,000 gallons
-# 42,000 gallons
-# 43,000 gallons
-# 44,000 gallons
-# 45,000 gallons
-# 46,000 gallons
-# Tank is either full or cannot add another 1,000 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 1
-# Tank radius: 10 feet
-# Tank depth: 20 feet
-# Max capacity: 46,998 gallons
-# Current water level: 46,000 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 5
-# Enter the rate (gallons of water/second) at which to drain the tank (1-5,000): 1000
-# 45,000 gallons
-# 44,000 gallons
-# 43,000 gallons
-# 42,000 gallons
-# 41,000 gallons
-# 40,000 gallons
-# 39,000 gallons
-# 38,000 gallons
-# 37,000 gallons
-# 36,000 gallons
-# 35,000 gallons
-# 34,000 gallons
-# 33,000 gallons
-# 32,000 gallons
-# 31,000 gallons
-# 30,000 gallons
-# 29,000 gallons
-# 28,000 gallons
-# 27,000 gallons
-# 26,000 gallons
-# 25,000 gallons
-# 24,000 gallons
-# 23,000 gallons
-# 22,000 gallons
-# 21,000 gallons
-# 20,000 gallons
-# 19,000 gallons
-# 18,000 gallons
-# 17,000 gallons
-# 16,000 gallons
-# 15,000 gallons
-# 14,000 gallons
-# 13,000 gallons
-# 12,000 gallons
-# 11,000 gallons
-# 10,000 gallons
-# 9,000 gallons
-# 8,000 gallons
-# 7,000 gallons
-# 6,000 gallons
-# 5,000 gallons
-# 4,000 gallons
-# 3,000 gallons
-# 2,000 gallons
-# 1,000 gallons
-# 0 gallons
-# Tank is either empty or cannot drain another 1,000 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 1
-# Tank radius: 10 feet
-# Tank depth: 20 feet
-# Max capacity: 46,998 gallons
-# Current water level: 0 gallons
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 6
-# Do you wish to create another water tank (Y or N)?: n
-# ---- Menu ----
-# 1 - Display water tank's state
-# 2 - Add water to tank
-# 3 - Withdraw water from tank
-# 4 - Fill water tank
-# 5 - Drain water tank
-# 6 - Enter inputs for another water tank
-# 7 - Exit the application
-# Enter your choice (1-7): 7
-# Do you wish to exit the application (Y or N)?: y
-
--- Assignments/AssignmentSet5/water_tank.py
+++ Assignments/AssignmentSet5/water_tank.py
@@ -8,11 +8,11 @@ import math
 
 # Water_Tank class with 4 instance attributes, all private: radius, depth, current water level & max water capacity in gallons
 class Water_Tank:
-    def __init__(self, radius, depth, current_water_level_gallons):
+    def __init__(self, radius, depth, __current_water_level_gallons=0):
         self.__radius = radius
         self.__depth = depth
-        self.__current_water_level_gallons = current_water_level_gallons
-        self.__max_water_capacity_gallons = __calc_max_water_capacity()
+        self.__current_water_level_gallons = __current_water_level_gallons
+        self.__max_water_capacity_gallons = self.__calc_max_water_capacity()
 
     # region getters
     @property
@@ -27,75 +27,79 @@ class Water_Tank:
     def current_water_level_gallons(self):
         return self.__current_water_level_gallons
     
+    @current_water_level_gallons.setter
+    def current_water_level_gallons(self, value):
+        self.current_water_level_gallons = value
+    
     @property
     def max_water_capacity_gallons(self):
         return self.__max_water_capacity_gallons
 
-# method to calculate and return the maximum water capacity of a tank
-def __calc_max_water_capacity(self):
-    # define constants
-    GALLONS_IN_ONE_CUBIC_FOOT = 7.48
-    # capacity = Pi * radius^2 * depth * 7.48 (1 cubic feet is approximately 7.48 gallons)
-    # capacity calculation results in value of type float. Use int() or math.floor() to convert to an integer
-    capacity = int(math.pi * (self.radius**2) * self.depth * GALLONS_IN_ONE_CUBIC_FOOT)
+    # method to calculate and return the maximum water capacity of a tank
+    def __calc_max_water_capacity(self):
+        # define constants
+        GALLONS_IN_ONE_CUBIC_FOOT = 7.48
+        # capacity = Pi * radius^2 * depth * 7.48 (1 cubic feet is approximately 7.48 gallons)
+        # capacity calculation results in value of type float. Use int() or math.floor() to convert to an integer
+        capacity = int(math.pi * (self.radius**2) * self.depth * GALLONS_IN_ONE_CUBIC_FOOT)
 
-    return capacity
+        return capacity
 
-# public method (with one parameter for the gallons of water to add) to add water to the tank if there is capacity
-def add_water_to_tank(self, gallons_water_to_add):
-    capacity_available_in_tank = self.max_water_capacity_gallons - self.current_water_level_gallons
-    # check if water_added is less than capacity left in tank
-    if gallons_water_to_add <= capacity_available_in_tank:
-        # if so, add the gallons_water_to_add to the current wanter in tank
-        self.current_water_level_gallons += gallons_water_to_add
-        # return a message confirming the gallons of water added & the tank’s current water level
-        return f"{gallons_water_to_add} were added to the tank. The tank's water level is now: {self.current_water_level_gallons}."f
-    
-    elif gallons_water_to_add > capacity_available_in_tank:
-        # return message indicating the tank will overflow, current water level, & maximum available capacity
-        return f"Adding {gallons_water_to_add} gallons to the tank will cause the tank to overflow. The tank's current water level is {self.current_water_level_gallons}. The maximum number of gallons that can be added is: {capacity_available_in_tank}."
+    # public method (with one parameter for the gallons of water to add) to add water to the tank if there is capacity
+    def add_water_to_tank(self, gallons_water_to_add):
+        capacity_available_in_tank = self.max_water_capacity_gallons - self.__current_water_level_gallons
+        # check if water_added is less than capacity left in tank
+        if gallons_water_to_add <= capacity_available_in_tank:
+            # if so, add the gallons_water_to_add to the current wanter in tank
+            self.__current_water_level_gallons += gallons_water_to_add
+            # return a message confirming the gallons of water added & the tank’s current water level
+            return f"{gallons_water_to_add:,} gallons were added to the tank.\nThe tank's water level is now: {self.__current_water_level_gallons:,} gallons."
+        
+        elif gallons_water_to_add > capacity_available_in_tank:
+            # return message indicating the tank will overflow, current water level, & maximum available capacity
+            return f"Adding {gallons_water_to_add:,} gallons to the tank will cause the tank to overflow.\nThe tank's current water level is {self.__current_water_level_gallons:,}.\nThe maximum number of gallons that can be added is: {capacity_available_in_tank:,}."
 
-# public method (with one parameter for the gallons of water to withdraw) to withdraw water from the tank
-def withdraw_water_from_tank(self, gallons_water_to_withdraw):
-    # check if the number of gallons to be withdrawn is available in the tank
-    if gallons_water_to_withdraw <= self.current_water_level_gallons:
-        # withdraw the water & return message confirming amount withdrawn, tank's current water level
-        self.current_water_level_gallons -= gallons_water_to_withdraw
-        return f"Withdrawing {gallons_water_to_withdraw} gallons from the tank. The current water level is now: {self.current_water_level_gallons}."
-    
-    # if the number of gallons to be withdrawn exceeds what the tank currently contains, do not withdraw the water
-    elif gallons_water_to_withdraw > self.current_water_level_gallons:
-    # return message indicating not enough water, & the max # of gallons that can be withdrawn from the tank 
-        return f"There are not {gallons_water_to_withdraw} gallons available in the water tank. The max amount available to withdraw is {self.current_water_level_gallons} gallons."
+    # public method (with one parameter for the gallons of water to withdraw) to withdraw water from the tank
+    def withdraw_water_from_tank(self, gallons_water_to_withdraw):
+        # check if the number of gallons to be withdrawn is available in the tank
+        if gallons_water_to_withdraw <= self.__current_water_level_gallons:
+            # withdraw the water & return message confirming amount withdrawn, tank's current water level
+            self.__current_water_level_gallons -= gallons_water_to_withdraw
+            return f"Withdrawing {gallons_water_to_withdraw:,} gallons from the tank.\nThe current water level is now: {self.__current_water_level_gallons:,}."
+        
+        # if the number of gallons to be withdrawn exceeds what the tank currently contains, do not withdraw the water
+        elif gallons_water_to_withdraw > self.__current_water_level_gallons:
+        # return message indicating not enough water, & the max # of gallons that can be withdrawn from the tank 
+            return f"There are not {gallons_water_to_withdraw:,} gallons available in the water tank.\nThe max amount available to withdraw is {self.__current_water_level_gallons:,} gallons."
 
-# public method (with one parameter for the gallons of water to add per second) to fill the tank at
-# a certain rate (i.e., gallons per second)
-def fill_water_per_second(self, gallons_to_add_per_second):
-    capacity_available_in_tank = self.max_water_capacity_gallons - self.current_water_level_gallons
-    # check if the # of gallons that will fill the tank in a second will not cause the tank to overflow
-    if gallons_to_add_per_second > capacity_available_in_tank:
-        self.current_water_level_gallons += gallons_to_add_per_second
-        # return a value of true
-        return True
-    # if gallons per second added will cause the tank to overflow, do not fill the tank
-    else:
-        # return a value of false.
-        return False
+    # public method (with one parameter for the gallons of water to add per second) to fill the tank at
+    # a certain rate (i.e., gallons per second)
+    def fill_water_per_second(self, gallons_to_add_per_second):
+        capacity_available_in_tank = self.max_water_capacity_gallons - self.__current_water_level_gallons
+        # check if the # of gallons that will fill the tank in a second will not cause the tank to overflow
+        if gallons_to_add_per_second <= capacity_available_in_tank:
+            self.__current_water_level_gallons += gallons_to_add_per_second
+            # return a value of true
+            return True
+        # if gallons per second added will cause the tank to overflow, do not fill the tank
+        else:
+            # return a value of false.
+            return False
 
-# public method (with one parameter for the gallons of water to drain per second) to drain the
-# tank at a certain rate (i.e., gallons per second)
-def drain_water_per_second(self, gallons_to_drain_per_second):
-    # check if the # of gallons to be drained is <= the current tank water level
-    if gallons_to_drain_per_second > self.current_water_level_gallons:
-        self.current_water_level_gallons -= gallons_to_drain_per_second
-        # return a value of true
-        return True
-    # if the number of gallons that will drain from the tank in a second is available in the tank, drain the tank. 
-    else:
-        # return a value of false.
-        return False
+    # public method (with one parameter for the gallons of water to drain per second) to drain the
+    # tank at a certain rate (i.e., gallons per second)
+    def drain_water_per_second(self, gallons_to_drain_per_second):
+        # check if the # of gallons to be drained is <= the current tank water level
+        if gallons_to_drain_per_second <= self.__current_water_level_gallons:
+            self.__current_water_level_gallons -= gallons_to_drain_per_second
+            # return a value of true
+            return True
+        # if the number of gallons that will drain from the tank in a second is available in the tank, drain the tank. 
+        else:
+            # return a value of false.
+            return False
 
-# __str__ method that returns relevant information about a water_tank object’s state (i.e.,
-# its attributes and their current values) as a string with appropriate labels and formatting.
-def __str__(self): # TODO
-    return f"Tank Radius: {self.radius}\nTank Depth: {self.depth}"
+    # __str__ method that returns relevant information about a water_tank object’s state (i.e.,
+    # its attributes and their current values) as a string with appropriate labels and formatting.
+    def __str__(self): # TODO
+        return f"Tank Radius: {self.radius:} feet\nTank Depth: {self.depth} feet\nMax Capacity: {self.__max_water_capacity_gallons:,} gallons\nCurrent Water Level: {self.__current_water_level_gallons:,} gallons"
