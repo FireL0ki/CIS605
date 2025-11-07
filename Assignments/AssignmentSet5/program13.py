22eafd1b096b6a00d7ec7de9762961df21a92384
Sif Oberon <muell758@d.umn.edu>
2025-11-06 18:33:16 -0600

    updates to final homework problem program

--- Assignments/AssignmentSet5/program13.py
+++ Assignments/AssignmentSet5/program13.py
@@ -1,10 +1,12 @@
 # Description: Allows user to choose from a menu to dispay information about championships from a .txt file
 # Developer: Sif Oberon
 # Date Created: 10.28.2025
-# Date Last Modified: 10.31.2025
+# Date Last Modified: 11.6.2025
 
 # import the Wimbledon_Champions class from the wimbledon_champions module
 from wimbledon_champions import Wimbledon_Champions
+from sys import exit
+
 
 # module level variable (for referencing a wimbledon champions object) initialized to “None”
 a_champion = None
@@ -15,80 +17,102 @@ def main():
     # call function that gets data for a champions object
     get_champions_data()
 
+# a function to get data from champions.txt and create a list of champions
 def get_champions_data():
+    champions_list = []
 
+    try:
+        # open champions.txt & read (r)
+        with open('champions.txt', 'r') as infile:
+            # read lines from the file and return as a list of strings
+            champions = infile.readlines()
 
-# 4. Has a function to
-# a. get data from champions.txt and create a list of champions
-# b. call the function (#5) that creates/instantiates a wimbledon champions object
+            # iterate over the lines in the file, putting the champions into a list
+            for champion in champions:
+                champion = champion.lower()
+                # add champions to the champion list and strip white space
+                champions_list.append(champion.strip())
 
-# 5. Has a function (with an appropriate parameter) to
-# a. create/instantiate a wimbledon champions object and assign it to the module level
-# variable
-# b. call the function (6) to display a menu
+    # error handling
+    except FileNotFoundError as e:
+        print(e)
 
-# 6. Has a function to
-# a. display the following menu of options:
-# 1) Display the number of times there have been back-to-back champions
-# 2) Display the number of times a player has won the championship
-# 3) Exit the application
-# b. get the user’s choice (i.e., 1-3) with an appropriate prompt and validation
-# c. call the function (#7) that calls the function associated with the user’s menu choice
+    except ValueError as e:
+        print(e)
 
-# 7. Has a function (with appropriate parameter) to
-# a. call the function associated with the user’s menu choice:
-# 1 – function #8
-# 2 – function #9
-# 3 – function #10
+    except Exception as e:
+        print(e)
 
-# 8. Has a function to
-# a. call the method that that returns the number of times there have been back-to-back
+    # call the function that creates/instantiates a wimbledon champions object
+    create_champion_object(champions_list=champions_list)
+
+# function (with an appropriate parameter) to create/instantiate a wimbledon champions object and assign it to the module level variable
+def create_champion_object(champions_list):
+    global a_champion
+    a_champion = Wimbledon_Champions(champions=champions_list)
+    # call the function to display a menu
+    display_menu()
+
+# function to display the  menu
+def display_menu():
+    print("\n----- Menu -----")
+    print("1) Display the number of times there have been back-to-back champions")
+    print("2) Display the number of times a player has won the championship")
+    print("3) Exit the application")
+
+    try:
+        while True:
+            user_selection = int(input("Enter our choice (1-3): "))
+            if 1 <= user_selection <= 3: break
+    except:
+        print("Input error")
+
+    # call method to get user selection
+    call_menu_function(user_selection)
+
+# method to call appropriate function based on user selection
+def call_menu_function(user_selection):
+    match user_selection:
+        case 1: get_number_of_back_to_back_champions()
+        case 2: get_number_of_championship_wins_for_player()
+        case 3: exit_application()
+
+# function to call the method that that returns the number of times there have been back-to-back
 # champions and display the result with appropriate wording
-# b. call the function (#6) to display the menu
+def get_number_of_back_to_back_champions():
+    number_championships = a_champion.get_number_back_to_back_champions()
+    print(f"Players have won back to back championships {number_championships} times")
+    # call the function to display the menu
+    display_menu()
 
-# 9. Has a function to
-# a. get user input (using an appropriate prompt) for a player’s name
-# b. call the method that returns the number of times a player has won the championship and
-# display the result with appropriate wording
-# c. call the function (#6) to display the menu
+# function to get user input (using an appropriate prompt) for a player’s name
+def get_number_of_championship_wins_for_player():
+    try:
+        while True:
+            player_name = input("Enter a player's name: ")
+            if player_name.lower() in a_champion.champions: 
+                break
+    except:
+        print("Input error")
+        get_number_of_championship_wins_for_player()
+    else:
+        # call the method that returns the number of times a player has won the championship and
+        # display the result with appropriate wording
+        number_championships = a_champion.get_player_championship_wins(player_name=player_name)
+        print(f"{player_name} has won the championship {number_championships} times")
+    # call the function to display the menu
+    display_menu()
 
-# 10. Has a function to
-# a. ask the user if they wish to exit the application
-# b. if yes, exit the application
-# c. else, call the function (#6) to display the menu
+# function to ask the user if they wish to exit the application
+def exit_application():
+    user_response = input("Do you wish to exit the application (Y / N)? ")
+    # if yes, exit the application
+    if user_response.lower() == "y":
+        print(f"Program End.")
+        exit()
+    # else, call the function to display the menu
+    else:
+        display_menu()
 
-# TODO sample output
-# Wimbledon Champions By LV
-# ---- Menu ----
-# 1 - Display the number of times there have been back-to-back champions
-# 2 - Display the number of times a player has won the championship
-# 3 - Exit the application
-# Enter your choice (1-3): 1
-# Players have won back-to-back championships 16 times
-# ---- Menu ----
-# 1 - Display the number of times there have been back-to-back champions
-# 2 - Display the number of times a player has won the championship
-# 3 - Exit the application
-# Enter your choice (1-3): 2
-# Enter a player's name: Steffi Graf
-# Steffi Graf has won the championship 7 times
-# ---- Menu ----
-# 1 - Display the number of times there have been back-to-back champions
-# 2 - Display the number of times a player has won the championship
-# 3 - Exit the application
-# Enter your choice (1-3): 2
-# Enter a player's name: Venus Williams
-# Venus Williams has won the championship 5 times
-# ---- Menu ----
-# 1 - Display the number of times there have been back-to-back champions
-# 2 - Display the number of times a player has won the championship
-# 3 - Exit the application
-# Enter your choice (1-3): 2
-# Enter a player's name: Coco Gauff
-# Coco Gauff has won the championship 0 times
-# ---- Menu ----
-# 1 - Display the number of times there have been back-to-back champions
-# 2 - Display the number of times a player has won the championship
-# 3 - Exit the application
-# Enter your choice (1-3): 3
-# Do you wish to exit the application (Y or N)?: y
+# main program call to start application
+main()
--- Assignments/AssignmentSet5/wimbledon_champions.py
+++ Assignments/AssignmentSet5/wimbledon_champions.py
@@ -1,7 +1,7 @@
 # Description: Class for a Wimbledon Champions object
 # Developer: Sif Oberon
 # Date Created: 10.28.2025
-# Date Last Modified: 10.31.2025
+# Date Last Modified: 11.6.2025
 
 # Wimebledon_Champions class with 1 instance attributes - champions (private)
 class Wimbledon_Champions:
@@ -13,30 +13,38 @@ class Wimbledon_Champions:
         return self.__champions
 
     # public method (with one parameter for the name of a tennis player) to check and return the number of times the player has won the championship
-    def get_player_championship_wins(player_name):
-        try:
-            # open champions.txt & read (r)
-            with open('champions.txt', 'r') as infile:
-                lines = infile.readlines()
-
-                # iterate over the lines in the file, finding all instances of the input player's name
-                for line in lines:
-                    count = 0
-                    if line == player_name:
-                        count + 1
-                        print(f"Count tracker: {count}")
-                return count
-            
-        except FileNotFoundError as e:
-            print(e)
-
-        except ValueError as e:
-            print(e)
-
-        except Exception as e:
-            print(e)
+    def get_player_championship_wins(self, player_name):
+        wins_count = 0
+        # for each champion in the champions list
+        for champion in self.__champions:
+            (print(f" Champion: {champion}"))
+            # check if the current index name matches the name entered by the user
+            if champion.lower() == player_name.lower():
+                # if so, add to the count
+                wins_count +=1
+        return wins_count
 
 
     # a public method to find and return the number of times there have been back-to-back champions
     def get_number_back_to_back_champions(self):
-        pass
+        back_to_back_count = 0
+        champions = self.champions
+
+        # initialize i at the starting index
+        i = 0
+        # iterate over the list of champions
+        while i < len(champions) - 1:
+            # compare current (index [i] and next winner to see if they match
+            if champions[i] == champions[i +1]:
+                # add to the count
+                back_to_back_count += 1
+                # skip to the next champion (since by comparing 1 & 2, champion 2 has already been counted if they were part of a back to back win)
+                # while the current iteration plus 1 (comparing the index following the initial, every other)is less than the length of champions - 1 (don't need to go beyond the second to last index)
+                # AND if the champion in the next index after the current is the same as the one after that
+                # ex: on index 1 (which has already been compared and matched index 2) and the next index [2] also matches index [3], that is 3 wins in a row, and the first back to back has already been counted
+                while i + 1 < len(champions) - 1 and champions[i + 1] == champions[i + 2]:
+                    # skip to the next champion / index
+                    i += 1
+            # move to next index
+            i += 1
+        return back_to_back_count
